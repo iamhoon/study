8724dbe20fb092aac482e831251a49a7d2e385ad
iamhoon <zown98@naver.com>
2020-07-31 22:17:18 +0900

    2?

--- src/nav_bar.js
+++ src/nav_bar.js
@@ -1,8 +1,7 @@
-import React, { Component } from 'react';
-import './App.css';
+import React from 'react';
 import './style.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faCamera } from "@fortawesome/free-solid-svg-icons"
 
 class Nav_bar extends React.Component{
     // constructor(props){
@@ -28,24 +27,23 @@ class Nav_bar extends React.Component{
             <body>
                 <nav class="navbar">
                     <div class="navbar_logo">
-                    <FontAwesomeIcon icon="smile" size="2x"/>
+                        <FontAwesomeIcon icon={faCamera} size="2x" />
                         <a href="">DreamCoding</a>
                     </div>
 
                     <ul class="navber_menu">
-                    <li><a href="">Home</a></li>
-                    <li><a href="">Gallery</a></li>
-                    <li><a href="">Weddings</a></li>
-                    <li><a href="">FAQ</a></li>
-                    <li><a href="">Bookings</a></li>
+                        <li><a href="">Home</a></li>
+                        <li><a href="">Gallery</a></li>
+                        <li><a href="">Weddings</a></li>
+                        <li><a href="">FAQ</a></li>
+                        <li><a href="">Bookings</a></li>
                     </ul>
                     <ul class="navbar_icons">
-                        <li><i class="fab fa-twitter"></i></li>
-                        <li><i class="fab fa-facebook-square"></i></li>
+                        <li><FontAwesomeIcon icon={faCamera} size="2x" /></li>
+                        <li><FontAwesomeIcon icon={faCamera} size="2x" /></li>
                     </ul>
                     <a href="#" class="navbar_toogleBtn">
-                        <i class="fas fa-bars"></i>
-
+                        <FontAwesomeIcon icon={faCamera} size="2x" />
                     </a>
                 </nav>
             </body>
@@ -53,14 +51,4 @@ class Nav_bar extends React.Component{
     }
 }
 
-class  App extends Component {
-  render(){
-  return (
-      <div className="App">
-        <Nav_bar/>
-      </div>
-    );
-  }
-}
-
-export default App;
+export default Nav_bar;
